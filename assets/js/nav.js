2a17dd37ffc8f84ecd71d02857716c21e7a42f4d
rbiiiii <foguenne.robin@gmail.com>
2020-05-28 10:57:46 +0200

    improve nav js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,41 +1,38 @@
-window.onload = function() {
 
-    // Main nav : keyboard navigation and accessibility
-    let navBtnOpen = document.querySelector('.site-nav__button');
-    let navCheckbox = document.querySelector('.site-nav__checkbox');
+// Main nav : keyboard navigation and accessibility
+var navBtnOpen = document.querySelector('.site-nav__button');
+var navCheckbox = document.querySelector('.site-nav__checkbox');
 
-    let openNav = function() {
-        navCheckbox.checked = true;
-        document.body.classList.add("nav-opened");
-        navBtnOpen.setAttribute('aria-expanded', true);
-    }
-    let closeNav = function() {
-        navCheckbox.checked = false;
-        document.body.classList.remove("nav-opened");
-        navBtnOpen.setAttribute('aria-expanded', false);
-    }
-    let toggleNav = function() {
-        if (navCheckbox.checked == false) {
-            openNav();
-        }
-        else {
-            closeNav();
-        }
-    };
-    navBtnOpen.addEventListener('click', toggleNav);
-    
-    // aria-expanded + body class toggle
-    navCheckbox.addEventListener('change', function() {
-        if (navCheckbox.checked == true) { 
-            navBtnOpen.setAttribute('aria-expanded', true);
-        } else {
-            navBtnOpen.setAttribute('aria-expanded', false);
-        
-        }
-    });
-    
-    // Escape key => close nav
-    document.addEventListener('keydown', function(e) {
-        if (e.keyCode == 27) { closeNav(); }
-    });
+var openNav = function() {
+    document.body.classList.add("nav-opened");
+    navBtnOpen.setAttribute('aria-expanded', true);
 }
+var closeNav = function() {
+    document.body.classList.remove("nav-opened");
+    navBtnOpen.setAttribute('aria-expanded', false);
+}
+var toggleNav = function() {
+    if (navCheckbox.checked == false) {
+        navCheckbox.checked = true;
+        openNav();
+    }
+    else {
+        navCheckbox.checked = false;
+        closeNav();
+    }
+};
+navBtnOpen.addEventListener('click', toggleNav);
+
+// aria-expanded + body class toggle
+navCheckbox.addEventListener('change', function() {
+    if (navCheckbox.checked == true) { 
+        openNav();
+    } else {
+        closeNav();
+    }
+});
+
+// Escape key => close nav
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 27) { closeNav(); }
+});
