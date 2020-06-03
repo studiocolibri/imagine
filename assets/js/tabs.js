6f93951f1fa54467022c40bb2735caec9d5862ec
rbiiiii <foguenne.robin@gmail.com>
2020-06-03 15:29:01 +0200

    tabs without js

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -2,7 +2,17 @@ var tabsBtns = document.getElementsByClassName("numero-section-two__tab__btn");
 var tabsImgs = document.getElementsByClassName("numero-section-two__tab__img");
 var tabsContents = document.getElementsByClassName("numero-section-two__tab__content");
 
-function openTab(num) {
+// add listeners on all tab links
+for (var y=0; y < tabsBtns.length; y++) {
+    tabsBtns[y].addEventListener('click', function(e) {
+        var num = this.dataset.num;
+        openTab(e, num);
+    })
+}
+
+// open the right tab and close others
+function openTab(e, num) {
+    e.preventDefault();
     for (var i=0; i< tabsContents.length; i++) {
         tabsContents[i].style.display = 'none';
     }
